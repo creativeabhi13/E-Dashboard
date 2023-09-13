f2b761dda34d15dcd79e910557a5927b63c2afa2
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-09-13 16:35:55 +0530

    Signup is happening via api and data is stored in database

--- front-end/src/components/Signup.jsx
+++ front-end/src/components/Signup.jsx
@@ -6,8 +6,17 @@ const Signup = () => {
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
 
-  const collectData =()=>{
+  const collectData = async ()=>{
     console.log(name,email,password);
+    let result= await fetch('http://localhost:5000/register',{
+    method:'post',
+    body: JSON.stringify({name,email,password}),
+    headers:{
+      'Content-Type':'application/json'
+    },
+  });  
+  result=await result.json()
+  console.log(result);
   }
   return (
     <>
@@ -16,7 +25,7 @@ const Signup = () => {
             Register
         </h1>
         <input className='inputBox' type="text" placeholder='Enter Name' value={name} onChange={(e)=>setName(e.target.value)}  />
-        <input className='inputBox' type="email" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input className='inputBox' type="text" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
         <input className='inputBox' type="password" placeholder='Enter Password' value={password} onChange={(e)=>setPassword(e.target.value)} />
         <button onClick={collectData} className='appButton' type="button">Sign Up</button>
     </div>
