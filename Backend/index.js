e6cda88f2eea0d8a7350d121f31ba04db2aa72f7
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-09-19 17:50:28 +0530

    pass remove from register and login and also fetch from database
    Make Route for API
    Pass data from postman
    Fetch Result from DB
    Remove password from Register
    and Login API
    Test Flow

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,28 @@ app.use(cors());
 app.post("/register",async (req, resp)=> {
     let user= new User(req.body);
     let result= await user.save();
+    result=result.toObject();
+    delete result.password 
     resp.send(result);
+})
+
+app.post("/login",async (req,resp)=>{
+    console.log(req.body);
+    if(req.body.password && req.body.email)
+    {
+        let user= await User.findOne(req.body).select("-password");
+        if(user)
+        {
+            resp.send(user);
+        }
+
+        else
+        {
+            resp.send({result:"No User Found"});
+        }
+    }
+    
+    
 })
 
 app.listen(5000);
