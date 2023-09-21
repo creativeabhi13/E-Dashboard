ef4fbe35f898c0d8cc89e53788f923e7470bd98f
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-09-21 17:16:47 +0530

    Basic form validation
    Check empty state
    Show error message
    Add Style
    Test flow with every input field

--- front-end/src/components/AddProduct.jsx
+++ front-end/src/components/AddProduct.jsx
@@ -1,36 +1,80 @@
-import React from 'react'
+import React from "react";
 
 const AddProduct = () => {
-    const [name,setName] = React.useState("");
-    const [price,setPrice] = React.useState("");
-    const [category,setCategory] = React.useState("");
-    const [company,setCompany] = React.useState("");
-  
-    const addProduct = async()=>{
-        console.log(name,price,category,company);
-        const userId = JSON.parse(localStorage.getItem('user'))._id; 
-        let result = await fetch("http://localhost:5000/add-product",{
-            method:'post',
-            body:JSON.stringify({name,price,category,company,userId}),
-            headers:{
-                "Content-Type":"application/json"
-            }
-        });
-        result = await result.json();
-        console.log(result);
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const [error, setError] = React.useState(false);
+
+  const addProduct = async () => {
+    console.log(!name);
+    if (!name || !price || !category || !company) {
+      setError(true);
+      return false;
     }
+
+    console.log(name, price, category, company);
+    const userId = JSON.parse(localStorage.getItem("user"))._id;
+    let result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify({ name, price, category, company, userId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.log(result);
+  };
   return (
-    <div className='product'>
-        <h1>Add Product</h1>
-        <input className='inputBox' type="text" placeholder='Enter Product Name' onChange={(e)=>{setName(e.target.value)}}  value={name}/>
-        <input className='inputBox' type="text" placeholder='Enter Product Price' onChange={(e)=>{setPrice(e.target.value)}} value={price} />
-        <input className='inputBox' type="text" placeholder='Enter Product Category' onChange={(e)=>{setCategory(e.target.value)}} value={category} />
-        <input className='inputBox' type="text" placeholder='Enter Product Company'  onChange={(e)=>{setCompany(e.target.value)}} value={company}/>
+    <div className="product">
+      <h1>Add Product</h1>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Product Name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        value={name}
+      />
+      {error && !name && <span className="invalid-input">Enter Valid Name</span>}
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Product Price"
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+        value={price}
+      />
+         {error && !price && <span className="invalid-input">Enter Valid Price</span>}
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Product Category"
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+        value={category}
+      />
+         {error && !category && <span className="invalid-input">Enter Valid category</span>}
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Enter Product Company"
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+        value={company}
+      />
+         {error && !company && <span className="invalid-input">Enter Valid Company</span>}
 
-        <button onClick={addProduct} className='appButton'>Add Product</button>
-
+      <button onClick={addProduct} className="appButton">
+        Add Product
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
