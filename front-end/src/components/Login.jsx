eaecb73daaaef1de1fe5c5752f68531b1866ffdf
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-09-21 12:47:56 +0530

    Test Api #16 api for login and use
    Test API with Postman
    Call API on login button click
    Set localStorage
    Redirect Page
    Update Navbar
    Test Flow

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 
 const Login =()=>{
     const [email,setEmail] =React.useState('');
     const [password,setPassword]=React.useState('');
-    const handleLogin=()=>{
-      console.log(email,password);
+    const navigate =useNavigate();
+    useEffect(()=>{
+      const auth = localStorage.getItem('user');
+      if(auth)
+      {
+        navigate("/")
+      }
+    })
+    const handleLogin= async ()=>{
+      console.log("email,password",email,password);
+      let result = await fetch('http://localhost:5000/login',{
+        method:'post',
+        body:JSON.stringify({email,password}),
+        headers:{
+          'Content-Type':'application/json'
+        }
+      });
+      result=await result.json();
+      console.log(result);
+        if(result.name){
+            localStorage.setItem("user",JSON.stringify(result));
+            navigate("/");
+        }
+        else{
+          alert("Please Enter Correct Email and Password");
+        }
+    
     }
 
     return(
         <div className='login'>
+            <h1>
+            Login
+        </h1>
             <input className='inputBox' type="text" name="" id="" placeholder='Enter Email'  onChange={(e)=>setEmail(e.target.value)} value={email} />
             <input className='inputBox' type="password" name="" id="" placeholder='Enter Passsword' onChange={(e)=>setPassword(e.target.value)} value={password} />
           <button onClick={handleLogin} className='appButton' type='button'>Login</button>
