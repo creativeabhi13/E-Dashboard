698690c1f898bd1a7fcc456f616bd811372b5957
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-10-12 16:06:16 +0530

    Make a new Route for API. Write code for update DB. for uopdate
    Make a new Route for API.
    Write code for update DB.
    Send data from the postman.

--- Backend/index.js
+++ Backend/index.js
@@ -54,7 +54,7 @@ app.get("/products",async(req,resp)=>{
 app.delete("/products/:id",async (req,resp)=>{
     const result = await Product.deleteOne({_id:req.params.id})
     resp.send(result);
-})
+});
 
 app.get("/products/:id",async (req,resp)=>{
     let result = await Product.findOne({_id:req.params.id});
@@ -66,6 +66,18 @@ app.get("/products/:id",async (req,resp)=>{
     {
         resp.send({result:"No Record Found."})
     }
-})
+});
+
+app.put("/products/:id",async (req,resp)=>{
+ 
+    let result = await Product.updateOne(
+        {_id:req.params.id},
+        {
+          $set :req.body
+        }
+    )
+    resp.send(result);
+});
+
 
 app.listen(5000);
